e5f0450406b6a4c86888aed2dd5213880ab9bfc2
olgazak11 <olga.zakharova76@gmail.com>
2017-11-20 14:20:15 +0100

    Solution for module2

---
+++ scripts/app.js
@@ -0,0 +1,72 @@
+(function(){
+'use strict';
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService)
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController (ShoppingListCheckOffService) {
+  var toBuyList = this;
+  toBuyList.items = ShoppingListCheckOffService.getItemsToBuyList();
+
+  toBuyList.buiedItem = function (itemIndex, item) {
+    ShoppingListCheckOffService.removeItemFromToBuyList(itemIndex);
+    ShoppingListCheckOffService.addItemToBoughtList(item);
+    //ShoppingListCheckOffService.getItemsToBoughtList();
+  }
+}
+
+AlreadyBoughtController.$inject= ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var boughtList = this;
+  boughtList.items = ShoppingListCheckOffService.getItemsToBoughtList();
+}
+
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  // List of items
+  var itemsToBuyList = [
+    {
+      name: "cockie",
+      quantity: 10
+    },
+    {
+      name: "milk",
+      quantity: 2
+    },
+    {
+      name: "coffie",
+      quantity:5
+    },
+    {
+      name: "ice-cream",
+      quantity: 6
+    },
+    {
+      name: "water",
+      quantity: 10
+    },
+  ];
+
+  var itemsBoughtList = [];
+
+  service.getItemsToBuyList = function () {
+    return itemsToBuyList;
+  };
+
+  service.getItemsToBoughtList = function () {
+    return itemsBoughtList;
+  };
+
+  service.removeItemFromToBuyList = function (itemIdex) {
+    itemsToBuyList.splice(itemIdex, 1);
+  };
+
+  service.addItemToBoughtList = function (item) {
+    itemsBoughtList.push(item);
+  }
+}
+
+})();
